c7a1d689071039ef95579e2025053e7151be596e
CtrlAltDeplete <alecs.gavyn@gmail.com>
2018-09-16 02:04:14 -0400

    Added testing.

---
+++ test_syllables.py
@@ -0,0 +1,23 @@
+from syllables import *
+import unittest
+
+
+class TestSyllables(unittest.TestCase):
+    words_to_test = {
+        'utterly': 3,
+        'vacuum': 2,
+        'spoil': 2,
+        'ruin': 2,
+        'civil': 2,
+        'service': 2,
+        'looking': 2
+    }
+
+    def test_word(self):
+        for key, value in self.words_to_test.items():
+            print(key)
+            self.assertEqual(syllablesInWord(key), value)
+
+
+if __name__ == '__main__':
+    unittest.main()
