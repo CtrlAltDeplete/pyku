00cf7377e6f18c94dcedf0483c0e48267670b1ca
gavyn <gavyn.partlow@gmail.com>
2018-09-18 11:09:51 -0400

    Added a better testing system.

--- test_syllables.py
+++ test_syllables.py
@@ -1,23 +1,15 @@
 from syllables import *
 import unittest
+from ddt import ddt
+from ddt import file_data
 
 
+@ddt
 class TestSyllables(unittest.TestCase):
-    words_to_test = {
-        'utterly': 3,
-        'vacuum': 2,
-        'spoil': 2,
-        'ruin': 2,
-        'civil': 2,
-        'service': 2,
-        'looking': 2,
-        'specified': 3
-    }
-
-    def test_word(self):
-        for key, value in self.words_to_test.items():
-            print(key)
-            self.assertEqual(syllablesInWord(key), value)
+    @file_data('testdata.txt')
+    def test_word(self, testData):
+        print(testData[0])
+        self.assertEqual(syllablesInWord(testData[0]), testData[1])
 
 
 if __name__ == '__main__':
